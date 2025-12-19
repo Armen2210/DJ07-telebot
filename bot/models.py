16a1b3fbf947adfe28ea66861f72781ce6513ce8
Армен Амбарцумян <armen0858@gmail.com>
2025-12-19 13:20:30 +0300

    updates

--- bot/models.py
+++ bot/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class TelegramUser(models.Model):
     user_id = models.BigIntegerField(unique=True)
-    username = models.CharField(max_length=255, blanc=True, null=True)
+    username = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- bot/views.py
+++ bot/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+# между скачанными модулями и личными ставится отступ: сначала скачанные(самые первые идут питоновские)
+from .models import TelegramUser
+from .serializers import TelegramUserSerializer
+
 
 # Create your views here.
+@api_view(['POST'])
+def register_user(request):
+    data = request.data
+    user, created = TelegramUser.objects.get_or_create(
+        user_id=data['user_id'],
+        defaults={
+            'username': data.get('username', ''),}
+    )
+    serializer = TelegramUserSerializer(user)
+    return Response(serializer.data)
+
